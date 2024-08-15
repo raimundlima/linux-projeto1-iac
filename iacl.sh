4f45cb4843a933baa2b26232593f331ad14259b3
raimundlima <raimundo_l@hotmail.com>
2024-08-15 15:33:19 +0000

    Arquivos IaC v.1

---
+++ iacl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "criando diretórios" 
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "criando grupo de usúarios" 
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "criando usuários" 
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd cintia -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd roberto -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+
+echo "especificando permissões dos diretórios" 
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec 
+chmod 777 /publico
+
+echo "fim...." 
